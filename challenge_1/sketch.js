5611dfbbc58312c96ebae7cfe0e99a0d890cdd90
kevinhb92 <leeh965@Kevinui-MBP.fios-router.home>
2015-09-24 23:07:25 -0400

    challenge_1 done

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -1,18 +1,38 @@
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
+	// create a place to draw (resclaed the background)
+	createCanvas(640, 450);
 	noStroke();
 	noLoop();
 }
 
 function draw() {
-	// clear the background
-	background(50, 50, 150);
+	// set a background color for challenge 1 color
+	background(50, 50, 50);
 
-	// set a fill color
-	fill(255, 255, 255);
+  // give a loop condition for 10 times for 10 square
+	for(var i = 0; i < 10; i++){
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+  //give a white stroke attribute to the square
+	strokeWeight(1);
+  stroke(255,255,255);
+
+	//give a fill color [ black to red spectrum - (255 / 10 = 25.5  )]
+	fill(25.5*i,0,0)
+
+	//draw 10 squares using loop
+	rect(10.5+63*i,200,52,52)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}

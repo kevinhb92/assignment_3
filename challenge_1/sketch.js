ac9a36c620d24e14974c56a7449243f9f478a66c
kevinhb92 <leeh965@Kevinui-MBP.fios-router.home>
2015-09-24 23:09:47 -0400

    organize the explanation

--- challenge_1/sketch.js
+++ challenge_1/sketch.js
@@ -10,17 +10,17 @@ function draw() {
 	background(50, 50, 50);
 
   // give a loop condition for 10 times for 10 square
-	for(var i = 0; i < 10; i++){
+	for(var i = 0; i < 10; i++); {
 
   //give a white stroke attribute to the square
 	strokeWeight(1);
   stroke(255,255,255);
 
-	//give a fill color [ black to red spectrum - (255 / 10 = 25.5  )]
-	fill(25.5*i,0,0)
+  //give a fill color [ black to red spectrum - (255 / 10 = 25.5  )]
+	fill(25.5*i,0,0);
 
 	//draw 10 squares using loop
-	rect(10.5+63*i,200,52,52)
+	rect(10.5+63*i,200,52,52);
 }
 
 

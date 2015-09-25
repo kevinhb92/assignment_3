4890c282e17d0ad58a66b390baaa601a33d4a961
kevinhb92 <leeh965@Kevinui-MBP.fios-router.home>
2015-09-24 23:40:17 -0400

    finished challenge_2

--- challenge_2/sketch.js
+++ challenge_2/sketch.js
@@ -1,6 +1,6 @@
 function setup() {
 	// create a place to draw
-	createCanvas(640, 360);
+	createCanvas(640, 460);
 	noStroke();
 	noLoop();
 }
@@ -9,29 +9,29 @@ function draw() {
 	// change the background color for challenge_2
 	background(130,130,170);
 
-	// give a condition to black circle not to have fill but black stroke
+	// give a condition to white circle not to have fill but white stroke
 	noFill();
-	strokeWeight(2);
-  stroke(0,0,0);
+	strokeWeight(1);
+  stroke(255,255,255);
 
-	//loop condition for 20 balck circle
-	 for(var black = 0; black < 20; black++) {
+	//loop condition for 20 white circle
+	 for(var white = 0; white < 20; white++) {
 
-	// draw the ellipse
-	ellipse(320, 180, 20+40*black, 20+40*black);
+	// draw the white ellipse
+	ellipse(320, 230, 20+40*white, 20+40*white);
 }
 
 
-// give a condition to white circle not to have fill but white stroke
+// give a condition to black circle not to have fill but black stroke
 noFill();
-strokeWeight(2);
-stroke(255,255,255);
+strokeWeight(1);
+stroke(0,0,0);
 
 //loop condition for 20 balck circle
- for(var white = 0; white < 20; white++) {
+ for(var black = 0; black < 20; black++) {
 
 // draw the ellipse
-ellipse(320, 180, 40+40*white, 40+40*white);
+ellipse(320, 230, 40+40*black, 40+40*black);
 }
 
 
